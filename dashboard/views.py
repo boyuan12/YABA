771f521da4d1cb1b45f488379434b6fb0b7458b1
Boyuan Liu <boyuanliu6@yahoo.com>
2022-07-17 23:13:17 -0700

    update transaction allow month and year params

--- dashboard/views.py
+++ dashboard/views.py
@@ -20,14 +20,28 @@ def index(request):
     })
 
 def transactions(request):
-    data = _get_transactions(request.session['access_token'], 8, 2021)
+    m = None
+    y = None
+    if request.GET.get("month") is not None:
+        m = int(request.GET.get("month"))
+        y = int(request.GET.get("year"))
+
+    if m == None:
+        data = _get_transactions(request.session['access_token'])
+    else:
+        data = _get_transactions(request.session['access_token'], m, y)
+
     spending = 0
     income = 0
-    for t in data["transactions"]:
-        if t["amount"] > 0:
-            spending += t["amount"]
-        else:
-            income += t["amount"] * -1
+    try:
+        for t in data["transactions"]:
+            if t["amount"] > 0:
+                spending += t["amount"]
+            else:
+                income += t["amount"] * -1
+    except KeyError:
+        return HttpResponse("Unable to fetch transaction data")
+
     return render(request, "dashboard/transactions.html", {
         "transactions": data,
         "spending": spending,
